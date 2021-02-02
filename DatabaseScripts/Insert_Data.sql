34b7d1f3780fc300178874bb1236d3ee2987a78a
Ronny Mahlangu <ronny@trans-africa.co.za>
2021-02-02 22:20:41 +0200

    added db script to insert data

---
+++ DatabaseScripts/Insert_Data.sql
@@ -0,0 +1,14 @@
+INSERT INTO [dbo].[Tracks]
+           ([Complexity])
+     VALUES('11110011100011001110011101'),('11111111001111110111100001')
+GO
+
+
+INSERT INTO [dbo].[VehicleAttributes]
+           ([VehicleTypeId]
+           ,[Accelaration]
+           ,[Breaking]
+           ,[Cornering]
+           ,[TopSpeed])
+     VALUES(1,8,3,4 ,9),(2,6,7,9,8)
+GO
