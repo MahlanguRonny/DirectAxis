10d9f6a910f768028a8da23314e8d1e2daa1ef5b
Ronny Mahlangu <ronny@trans-africa.co.za>
2021-02-03 00:13:41 +0200

    added word document describing application flow

--- DatabaseScripts/Insert_Data.sql
+++ DatabaseScripts/Insert_Data.sql
@@ -1,6 +1,12 @@
 INSERT INTO [dbo].[Tracks]
            ([Complexity])
-     VALUES('11110011100011001110011101'),('11111111001111110111100001')
+     VALUES('11110011100011001110011101'),('11111111001111110111100001','11001')
+GO
+
+
+INSERT INTO [dbo].[CarType]
+           ([Description])
+     VALUES ('CORV'), ('GTR'),('SUV')
 GO
 
 
@@ -10,5 +16,8 @@ INSERT INTO [dbo].[VehicleAttributes]
            ,[Breaking]
            ,[Cornering]
            ,[TopSpeed])
-     VALUES(1,8,3,4 ,9),(2,6,7,9,8)
+     VALUES(1,8,3,4 ,9),(2,6,7,9,8),(3,9,9,4,8)
 GO
+
+
+
